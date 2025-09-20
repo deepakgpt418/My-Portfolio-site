c09e2bfa321b82375cbb3c9d9699c5a7ff711cda
Deepak Gupta <114178279+deepakgpt418@users.noreply.github.com>
2025-09-20 19:44:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Deepak Gupta",
+  title: "Frontend Developer || Web Designer ",
+  email: "deepakgupta.wevdep@gmail.com",
+  gitHub: "deepakgpt418",
+  instagram: "formwalasite",
+  linkedIn: "https://medium.com/@deepakguptacomputerDeepak gupta",
+  medium: "Formwalasite",
+  twitter: "",
+  youTube: "https://www.youtube.com/@formwalasite",
 };
 
 const primaryColor = "#4E567E";
